6f799bf64ee4bc47a20bd0904d157c47747e327f
Stefan Siegl <stesie@brokenpipe.de>
2018-10-01 02:47:37 +0200

    don't allow to change colors so we actually loose state

--- NullAnimator.cpp
+++ NullAnimator.cpp
@@ -17,21 +17,38 @@ void NullAnimator::commit() {
 }
 
 void NullAnimator::setRed(uint8_t red) {
+  if (red == 0 && green == 0 && blue == 0) {
+    red = 1; // don't actually turn off the last LED
+  }
+
   updateColors(pack(this->red, green, blue), pack(red, green, blue));
   this->red = red;
 }
 
 void NullAnimator::setGreen(uint8_t green) {
+  if (red == 0 && green == 0 && blue == 0) {
+    green = 1; // don't actually turn off the last LED
+  }
+
   updateColors(pack(red, this->green, blue), pack(red, green, blue));
   this->green = green;
 }
 
 void NullAnimator::setBlue(uint8_t blue) {
+  if (red == 0 && green == 0 && blue == 0) {
+    blue = 1; // don't actually turn off the last LED
+  }
+
   updateColors(pack(red, green, this->blue), pack(red, green, blue));
   this->blue = blue;
 }
 
 void NullAnimator::updateColors(uint32_t oldColor, uint32_t newColor) {
+  if (oldColor == 0) {
+    // don't update if old color was black, otherwise all LEDs would be enabled
+    return;
+  }
+
   for (uint8_t y = 0; y < driver->height(); y ++) {
     for (uint8_t x = 0; x < driver->width(); x ++) {
       if (driver->getPixel(x, y) == oldColor) {
@@ -39,4 +56,6 @@ void NullAnimator::updateColors(uint32_t oldColor, uint32_t newColor) {
       }
     }
   }
+
+  commit();
 }
